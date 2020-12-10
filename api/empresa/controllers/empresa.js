e3c87dc1925eddd4151105df008416e79c829554
santy patiño <santyp638@gmail.com>
2020-12-10 10:37:21 -0500

    ya casi xd

--- api/empresa/controllers/empresa.js
+++ api/empresa/controllers/empresa.js
@@ -1,8 +1,40 @@
 'use strict';
-
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    async ve(ctx) {
+        const user = ctx.state.user
+
+        let empresa = await strapi.services.empresa.find({
+            user: user.id,
+        });
+        return empresa;
+    },
+
+    async create(ctx) {
+        let entity;
+        const user = ctx.state.user;
+
+        if (ctx.is('multipart')) {
+            const { data, files } = parseMultipartData(ctx);
+            data.user = user.id;
+
+            entity = await strapi.services.empresa.create(
+                data, {
+                    files,
+                });
+        } else {
+            const data = ctx.request.body;
+            data.user = user.id;
+            entity = await strapi.services.empresa.create(data);
+        }
+
+        return sanitizeEntity(entity, {
+            model: strapi.models.empresa
+        });
+    },
+};
