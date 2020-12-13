a43a8201c5da1db23dd2292c4aef1958f9df2782
santy patiño <santyp638@gmail.com>
2020-12-12 22:55:48 -0500

    categorias

--- api/categorias/controllers/categorias.js
+++ api/categorias/controllers/categorias.js
@@ -5,4 +5,39 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+
+    async ve(ctx) {
+        const user = ctx.state.user
+
+        let categorias = await strapi.services.categorias.find({
+            user: user.id,
+        });
+        return categorias;
+    },
+
+    async create(ctx) {
+        let entity;
+        const user = ctx.state.user;
+
+        if (ctx.is('multipart')) {
+            const { data, files } = parseMultipartData(ctx);
+            data.user = user.id;
+
+            entity = await strapi.services.categorias.create(
+                data, {
+                    files,
+                });
+        } else {
+            const data = ctx.request.body;
+            data.user = user.id;
+            entity = await strapi.services.categorias.create(data);
+        }
+
+        return sanitizeEntity(entity, {
+            model: strapi.models.categorias
+        });
+    },
+
+
+};
